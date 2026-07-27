e16334573f442c6df50f34dbef613fc92fe46383
farmsilo-dev <contato@farmsilo.net>
2026-07-26 21:29:26 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import path from "path";
 
 export default defineConfig({
   server: {
@@ -8,6 +9,7 @@ export default defineConfig({
     port: 8080,
   },
   plugins: [
+    tanstackStart(),
     react(),
   ],
   resolve: {
